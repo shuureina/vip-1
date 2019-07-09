610a308c68db85ec7c863165ef1fcc75852008f7
shuureina <1273105877@qq.con>
2019-07-09 23:10:06 +0800

    submit 3.2

--- src/script/js/registor.js
+++ src/script/js/registor.js
@@ -1,18 +1,34 @@
 $(function() {
+    const $ctgroup1 = $('.control-group1');
+    const $mbname = $('#J_mobile_name');
+    const $pwd = $("#J_mobile_pwd");
+    const $tiptext = $('.control-group1 .u-input-tip .text');
     $('#reg_mobile_form').validate({
         rules: {
             loginName: {
                 required: true,
                 minlength: 2,
                 remote: { //把前端loginName 传给后端
-                    url: 'http://10.31.158.73:8080/vip/php/registor.php',
-                    type: 'post'
+                    url: 'http://10.31.158.73:8080/vip/php/registor.php', //后台处理程序
+                    type: 'post', //数据发送方式
+                    data: 'json', //接收数据的格式
+                    dataType: { //要传递的数据
+                        username: function() {
+                            return $('#J_mobile_name').val();
+                        }
+                    }
                 }
+            },
+            pvcode: {
+                required: true,
+                minlength: 6,
+                maxlength: 6
             },
             password: {
                 required: true,
                 minlength: 5
             },
+
             confirmPassword: {
                 required: true,
                 minlength: 5,
@@ -20,11 +36,21 @@ $(function() {
             },
             agree: "required"
         },
+        errorPlacement: function(error, element) {
+            $(element).closest('.control-group').find('.text').append(error);
+        },
+        errorElement: "span",
+        errorClass: 'text',
         messages: {
 
             loginName: {
-                required: "请输入用户名",
+                required: "请输入11位手机号码",
                 minlength: "用户名必需由两个字母组成"
+            },
+            pvcode: {
+                required: "请输入6位验证码(只要是6个数字就可以)",
+                minlength: "验证码输入错误",
+                maxlength: "验证码输入错误"
             },
             password: {
                 required: "请输入密码",
@@ -36,47 +62,33 @@ $(function() {
                 equalTo: "两次密码输入不一致"
             },
             agree: "请接受我们的声明"
+        },
+        submitHander: function(form) {
+            location.assign('http://10.31.158.73:8080/vip/src/login.html');
+            form.submit();
         }
+
     });
+
 });
 $.validator.setDefaults({
-    /*添加校验成功后的执行函数--修改提示内容，并为正确提示信息添加新的样式(默认是valid)*/
-    success: function() {
+    /*添加校验成功后的执行函数--修改提示内容*/
+    success: function(span) {
         $('.u-input-success').addClass('show');
+        $.ajax({
+            type: "post",
+            url: "http://10.31.158.73:8080/vip/php/registor.php",
+            data: "json",
+            dataType: {
+                username: function() {
+                    return $('#J_mobile_name').val();
+                },
+                pwd: $pwd.val()
+
+            },
+            success: function(response) {
+
+            }
+        });
     }
 });
-$('#J_mobile_name').on('blur', function() {
-$('.u-input-tip .text').html('请输入')
-});
-
-
-// const $username = $('#J_mobile_name');
-//     const $pwd = $("#J_mobile_pwd");
-//     const $checkpwd = $("#J_mobile_confirm_pwd");
-//     const $idcode = $("#J_mobile_code");
-//     const phpurl = 'http://10.31.158.73:8080/vip/php/';
-//     $username.on('blur', function() {
-
-//         $.ajax({
-//             type: "post",
-//             url: phpurl + "registor.php",
-//             data: {
-//                 username: $username.value
-//             },
-//             dataType: "json",
-//             success: function(data) {
-//                 if (!data) {
-//                     $('.control-group1 .u-input-success').css({
-//                         visibility: visible,
-//                         opacity: 1
-//                     })
-//                 } else {
-//                     $('.control-group1 .u-input-warning').css({
-//                         visibility: visible,
-//                         opacity: 1
-//                     })
-//                     $(".u-input-tip .text").html('用户名已存在');
-//                 }
-//             }
-//         });
-//     });
